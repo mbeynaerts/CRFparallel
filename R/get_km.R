0acc278a93acb75d8734f8284e6bd6c7d0a70599
Miguel Beynaerts <miguel.beynaerts@gmail.com>
2026-04-30 14:56:15 +0200

    Code for getting marginal KM-estimates

---
+++ R/get_km.R
@@ -0,0 +1,19 @@
+get_km <- function(object) {
+  stopifnot(inherits(object, "biSurv"))
+  stopifnot(isTRUE(attr(object, "bernstein")))
+
+  t1 <- object$t1
+  t2 <- object$t2
+  event1 <- object$event1
+  event2 <- object$event2
+
+  # Calculate the censoring time and event indicator for the combined censoring variable
+  c <- max(t1, t2)
+  event_c <- 1 - event1 * event2
+
+  km1 <- survival::survfit(survival::Surv(t1, event1) ~ 1)
+  km2 <- survival::survfit(survival::Surv(t2, event2) ~ 1)
+  kmc <- survival::survfit(survival::Surv(c, event_c) ~ 1)
+
+  return(list(km1 = km1, km2 = km2, kmc = kmc))
+}
